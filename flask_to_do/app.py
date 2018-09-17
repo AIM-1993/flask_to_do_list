0174aba311c3e5dcd0f4ac3619dfeea8cd4d2199
AIM1993 <244018719@qq.com>
2018-09-17 19:58:12 +0800

    在window平台完善了前端页面效果

--- flask_to_do/app.py
+++ flask_to_do/app.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-
 @app.route('/about')
 def about():
-    return render_template('about.html', name=about)
-
+    return render_template('about.html')
 
 @app.route('/edit')
 def edit():
     return render_template('edit.html')
 
+@app.route('/delete')
+def delete():
+    return redirect(url_for('home'))
+
+@app.route('/mark')
+def mark():
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
