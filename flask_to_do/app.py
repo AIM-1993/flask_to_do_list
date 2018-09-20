43717a10637b9109c1c8024ec862f3dfd8340ccb
AIM1993 <244018719@qq.com>
2018-09-20 22:48:17 +0800

    完成了增加数据后端逻辑的编辑，并新增了config.py,并.gitignore了。

--- flask_to_do/app.py
+++ flask_to_do/app.py
@@ -1,28 +1,68 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, make_response
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
 
-@app.route('/about')
+
+class Todo(db.Model):
+    __tablename__ = "todolist"
+    id = db.Column(db.Integer, primary_key=True)
+    thing = db.Column(db.String(30), unique=False)
+    done = db.Column(db.Boolean(), default=False)
+
+    def __repr__(self):
+        return self.thing, self.done
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == "POST":
+        if request.form.get('backlog') == '':
+            content = {'list': Todo, 'warning': "Please input something"}
+            resp = redirect(url_for('home'))
+            resp.headers['X-Something'] = 'A value'
+            return resp
+        else:
+            a_new_data = Todo(thing = request.form.get('backlog'), done=False)
+            db.session.add(a_new_data)
+            db.session.commit()
+            content = {'message': "Update Complete."}
+            resp = make_response(render_template('home.html'))
+            resp.headers['X-Something'] = 'A value'
+            return resp
+    elif request.method == "GET":
+        content = {'list': Todo}
+        return render_template('home.html')
+
+
+@app.route('/about/')
 def about():
     return render_template('about.html')
 
-@app.route('/edit')
+@app.route('/edit/', methods=['GET', 'POST'])
 def edit():
     return render_template('edit.html')
 
-@app.route('/delete')
+@app.route('/delete/')
 def delete():
     return redirect(url_for('home'))
 
-@app.route('/mark')
+@app.route('/mark/')
 def mark():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
     app.run(debug=True)
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     return make_response(return render_template('error.html'), 404)
+#     resp.headers['X-Something'] = 'A value'
+#     return resp
