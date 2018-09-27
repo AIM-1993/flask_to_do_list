5bf123fcf766e7cca2b7120c39e82423830dbc3c
AIM-1993 <244018719@qq.com>
2018-09-27 12:28:22 +0800

    for test

---
+++ config.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:xza3363199--AIMI@localhost/newsiteflask?charset=utf8"
+SQLALCHEMY_TRACK_MODIFICATIONS=False
